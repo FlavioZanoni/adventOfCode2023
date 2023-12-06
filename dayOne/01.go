c68b77e987ed4cb05cd7f68da761f6a213b95e88
FlavioZanoni <flaviozg888@gmail.com>
2023-12-06 17:46:19 -0300

    Second part day one
    My advent is broken

--- dayOne/01.go
+++ dayOne/01.go
@@ -11,7 +11,7 @@ import (
 func main() {
 
 	var sumTotal int32 = 0
-	var reg, err = regexp.Compile("[0-9]")
+	var reg, err = regexp.Compile(`[1-9]|one|two|three|four|five|six|seven|eight|nine`) // donno how to do this better
 
 	if err != nil {
 		fmt.Println("Error compiling regex:", err)
@@ -30,13 +30,21 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		matches := reg.FindAllString(line, -1)
+		fmt.Println("------------")
+
+		fmt.Println("Line:", line)
 
 		if len(matches) == 0 {
 			continue
 		} else {
-			sum := matches[0] + matches[len(matches)-1]
 
-			conversion, err := strconv.Atoi(sum)
+			fmt.Println("Matches:", matches)
+
+			concat := concatNumbers(matches[0], matches[len(matches)-1])
+			conversion, err := strconv.Atoi(concat)
+
+			fmt.Println("Concat:", concat)
+			fmt.Println("Conversion:", conversion)
 
 			if err != nil {
 				fmt.Println("Error converting string to int:", err)
@@ -44,10 +52,7 @@ func main() {
 			}
 
 			sumTotal += int32(conversion)
-
-			fmt.Println(sum)
 		}
-
 	}
 
 	fmt.Println("Total:", sumTotal)
@@ -55,4 +60,34 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
 	}
+}
+
+func concatNumbers(first string, last string) string {
+	mapOfNumbers := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
+	result := ""
+
+	if value, ok := mapOfNumbers[first]; ok {
+		result += value
+	} else {
+		result += first
+	}
+
+	if value, ok := mapOfNumbers[last]; ok {
+		result += value
+	} else {
+		result += last
+	}
+
+	return result
 }
